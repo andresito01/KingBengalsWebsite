13c5876f75b31878f5785d54192bc4aa84430675
Joshi Ventura <hyrein@hotmail.com>
2022-11-18 10:45:18 -0800

    #JIRA/KB-20 #Create the Breed Information & History Header This add the the "Breed Information & History" under Navigation header. Also it adds the bottom images, "Spotted" and "Marbleed" images.

--- client/src/pages/BreedInfo.tsx
+++ client/src/pages/BreedInfo.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import WebNavHeader from '../components/WebNavHeader';
-import "./styles/BreedInfo.css"
+import React from "react";
+import WebNavHeader from "../components/WebNavHeader";
+import "./styles/BreedInfo.css";
+import topImage from "../images/SpottedVsMarbled.png";
+import spotted from "../images/SpottedBengals.png";
+import marbled from "../images/MarbledBengals.png";
 
 const BreedInfo = () => {
-
   return (
-    <div className='breedInfoPage'>
+    <div className="breedInfoPage">
       <WebNavHeader />
+      <BreedInfoTopPic />
+      <div className="bottomContainer">
+        <SpottedBengals />
+        <MarbledBengals />
+      </div>
     </div>
-  )
-}
+  );
+};
+
+const BreedInfoTopPic = () => {
+  return (
+    <div className="topImageContainer">
+      <img className="topPicture" src={topImage} alt="Breed Information" />
+      <div className="breedInfoTxt">Breed Information and History</div>
+    </div>
+  );
+};
+const SpottedBengals = () => {
+  return (
+    <img src={spotted} alt="Breed Information" style={{ float: "left" }} />
+  );
+};
+const MarbledBengals = () => {
+  return (
+    <img src={marbled} alt="Breed Information" style={{ float: "right" }} />
+  );
+};
 
 export default BreedInfo;
