068d57dff41304e0993883a988393d3b2037eb15
1caiser <1caiser@gmail.com>
2023-02-20 01:54:05 -0800

    add intro, add picture, rearrange spot-vs-marb

--- client/src/pages/BreedInfo.tsx
+++ client/src/pages/BreedInfo.tsx
@@ -4,15 +4,33 @@ import "./styles/BreedInfo.css";
 import topImage from "../images/SpottedVsMarbled.png";
 import spotted from "../images/SpottedBengals.png";
 import marbled from "../images/MarbledBengals.png";
+import redstar from "../images/1024px-Paintedcats_Red_Star_standing.jpg";
 
 const BreedInfo = () => {
   return (
     <div className="breedInfoPage">
+      
       <WebNavHeader />
+      <div className='backgroundLayer'>
+        <div className = "bengalsIntroContainer">
+        <img className = "bengalsIntroPic" src={redstar} alt="" />
+          <div className = "bengalsIntroText"><br /><br />
+          The Bengal breed of cats gets their name from the Asian leopard cat, 
+          taxonomic name: <i>Prionailurus bengalensis</i>, from which they 
+          are bred together with domestic cat breeds and usually again with
+          cat breeds that have a more friendly nature. <br/><br/>
+          With their appearance of a miniature leopard, the initial goal was to 
+          produce a cat breed to serve as an alternative to keeping wild cats as 
+          pets. Bengals are strikingly known for being the only domestic cat 
+          breed to feature "rosette" or spotted markings, though their fur can
+          sport an assortment of different kinds of patterns.
+          </div>
+        </div>
+      </div>
       <BreedInfoTopPic />
       <div className="bottomContainer">
-        <SpottedBengals />
-        <MarbledBengals />
+        <img src = {spotted} alt="" style={{float:"left", width:500 }} />
+        <img src = {marbled} alt="" style={{float:"right", width:500 }} />
       </div>
     </div>
   );
@@ -24,16 +42,6 @@ const BreedInfoTopPic = () => {
       <img className="topPicture" src={topImage} alt="Breed Information" />
       <div className="breedInfoTxt">Breed Information and History</div>
     </div>
-  );
-};
-const SpottedBengals = () => {
-  return (
-    <img src={spotted} alt="Breed Information" style={{ float: "left" }} />
-  );
-};
-const MarbledBengals = () => {
-  return (
-    <img src={marbled} alt="Breed Information" style={{ float: "right" }} />
   );
 };
 
