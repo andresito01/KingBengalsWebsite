86e2280194eb87e25fc8a8925b061f4de0725c98
student <student@localhost>
2023-03-02 13:34:39 -0800

    Comment out emailjs temporarily

--- client/src/pages/ContactUs.tsx
+++ client/src/pages/ContactUs.tsx
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react';
-import emailjs from '@emailjs/browser';
+//import emailjs from '@emailjs/browser';
 import WebNavHeader from '../components/WebNavHeader';
 import ContactUsCSS from "./styles/ContactUs.module.css";
 
@@ -7,24 +7,33 @@ import ContactUsCSS from "./styles/ContactUs.module.css";
 const ContactUs = () => {
 
   const form = useRef();
-
+  
   const sendEmail = (e: React.SyntheticEvent) => {
     e.preventDefault();
-
+    /*
     emailjs.sendForm('service_gl7djk6', 'template_n8dohwi', form.current, 'Fnz9NuRAzufrPHXEs')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
-      });
+      });*/
       
   };
 
-
+  //Belongs in the return statement
+      /*<form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>*/
   
   return (
     
-    <><div className={ContactUsCSS.ContactUs}>
+    <div className={ContactUsCSS.ContactUs}>
       <WebNavHeader />
       <div className={ContactUsCSS.bg}>
         <div className={ContactUsCSS.mainContentContainer}>
@@ -35,20 +44,13 @@ const ContactUs = () => {
             <br></br>
             Email: KingbengalsCattery@hotmail.com<br></br>
           </p>
-
-          <div className={ContactUsCSS.contactButton}>Get In Touch</div><br></br></div></div></div>
-          
-          
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form></>
+          <div className={ContactUsCSS.contactButton}>
+            Get In Touch
+          </div><br></br>
+        </div>
+      </div>
     
+    </div>
   );
 };
 
