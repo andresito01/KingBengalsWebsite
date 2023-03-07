d958d2376983f28d4217f5c0bd8cf3166948ee8d
MehakdeepSingh07 <mehakrangi99@gmail.com>
2023-03-07 01:26:27 -0800

    Updated OwnersInfo page with 'Products we recommend' container and background and  new header image.

--- client/src/pages/OwnersInfo.tsx
+++ client/src/pages/OwnersInfo.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import OwnersInfoCSS from "./styles/OwnersInfo.module.css";
 import WebNavHeader from "../components/WebNavHeader";
 
-/*******************************Owner's Info***************************** */
+/*******************************Owner's Info***************************** Note: A link on line 15 for pro-recomm*/
+/**/
 
 const OwnersInfo = () => {
 
@@ -12,7 +13,11 @@ const OwnersInfo = () => {
       <img className={OwnersInfoCSS.img1} alt='headerImage' src={require("../images/bencat.jpg")}/>
       <div className={OwnersInfoCSS.backgroundLayer}>
         <div className={OwnersInfoCSS.headerContainerProducts}>
-          <h1 className={OwnersInfoCSS.underline}>Products we Recommend</h1>
+          <h1 className={OwnersInfoCSS.bold}> Products we Recommend </h1>
+          <h3>____________________________________________________________________________________________</h3>
+          <h3> Below is a list of/link to products we recommend for your kitten.</h3>
+          <h3> From toys, treats, catteries?, bedding, and training accessories? </h3>
+          <h2> o==o  Link/List  o==o </h2>
         </div>
       </div>
     </div>
