2b0dec02f2efa33dadea0dbd67393ff136e7624a
MehakdeepSingh07 <mehakrangi99@gmail.com>
2023-03-09 04:41:45 -0800

    Made aesthetic improvements in OwnersInfo.

--- client/src/pages/OwnersInfo.tsx
+++ client/src/pages/OwnersInfo.tsx
@@ -15,29 +15,18 @@ const OwnersInfo = () => {
 
       <WebNavHeader/>
 
-      {/*<img className={OwnersInfoCSS.img1} alt='headerImage' src={require("../images/bencat - OwnersInfo header image.jpg")}/>
-      <div className={OwnersInfoCSS.headerContainerOwnersInfo} style={fontSizeHeader}>
-       <h1> Owner's Info </h1>
-      </div>*/}
-
       <OwnersInfoPageHeader/>
 
-      <div className={OwnersInfoCSS.backgroundLayer}> 
-        {/*<div className={OwnersInfoCSS.headerContainerProducts}>
-          <h1 className={OwnersInfoCSS.bold}> Products We Recommend </h1>
-          <h3>___________________________________________________________________________________________________________________</h3>
-          <h3> Below is a list of/link to products we recommend for your kitten. From toys, treats, catteries?, bedding, and training accessories? </h3>
-          {/*<h3> From toys, treats, catteries?, bedding, and training accessories? </h3> }
-          <h2> o==o  Link/List  o==o </h2>
-        </div>*/}
+     {/*<div className={OwnersInfoCSS.backgroundLayer}> */}
 
-        <ProductsRecommend/>
+      <ProductsRecommend/>
 
-        <KittenDevelopment/>
+      <KittenDevelopment/>
 
-        <PrepareForKitten/>
+      <PrepareForKitten/>
 
-      </div>
+
+
     </div>
   )
 }
@@ -68,18 +57,9 @@ const ProductsRecommend = () => {
       {/*<h3> From toys, treats, catteries?, bedding, and training accessories? </h3> */}
       <h2> A Link or List </h2>
     </div>
-
-    /*<div className={OwnersInfoCSS.procuctsRecommend}>
-        <h1 className={OwnersInfoCSS.bold}> Products We Recommend </h1>
-          <h3>___________________________________________________________________________________________________________________</h3>
-          <h3> Below is a list of/link to products we recommend for your kitten. From toys, treats, catteries?, bedding, and training accessories? </h3>
-          {/*<h3> From toys, treats, catteries?, bedding, and training accessories? </h3> }
-          <h2> o==o  Link/List  o==o </h2>*/
-    /*</div>*/
   );
 
 }
-
 
 const KittenDevelopment = () => {
   return (
@@ -101,7 +81,7 @@ const KittenDevelopment = () => {
 const PrepareForKitten = () => {
   return (
   <div className={OwnersInfoCSS.containerPrepareForKitten}>
-    <div className={OwnersInfoCSS.containerPrepare}>
+    <div className={OwnersInfoCSS.containerPrepare2}>
       <h1 className={OwnersInfoCSS.bold}> Preparing For Your Kitten </h1>
       <h3> ___________________________________</h3>
       <h3> When you pick up your adorable kitten, it will go smoothly if you anticipate and prepare for the coming of that small bundle of joy and energy into your house.
