9acf7c072e327e0b0e5927ae971babccc4791e81
MehakdeepSingh07 <mehakrangi99@gmail.com>
2023-05-11 16:02:31 -0700

    Linked WebNavHeaderCSS file to WebNavUnderLayer.tsx

--- client/src/components/NavBarUnderLayer.tsx
+++ client/src/components/NavBarUnderLayer.tsx
@@ -1,7 +1,7 @@
 import React, { useState} from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
-import NavBarUnderLayerCSS from "./styles/WebNavHeader.module.css"
+import WebNavHeaderCSS from "./styles/WebNavHeader.module.css"
 const NavBarUnderLayer = () => {
 
   //let navigate = useNavigate();
@@ -20,64 +20,64 @@ const NavBarUnderLayer = () => {
 
   return (
     
-    <div className={NavBarUnderLayerCSS.headerContainer2}>
-      {/*<img className={Mobile ? NavBarUnderLayerCSS.logoImgResponsive : NavBarUnderLayerCSS.logoImg} id="leftHeaderImg" alt='logo' src={require("../images/KB logo.png")} />*/}
-      <div className={Mobile ? NavBarUnderLayerCSS.middleResponsive : NavBarUnderLayerCSS.middle} id="middle">
-        <div className={NavBarUnderLayerCSS.headerTitleContainer}>
-          <div className={NavBarUnderLayerCSS.headerKingBengals}>
+    <div className={WebNavHeaderCSS.headerContainer2}>
+      {/*<img className={Mobile ? WebNavHeaderCSS.logoImgResponsive : WebNavHeaderCSS.logoImg} id="leftHeaderImg" alt='logo' src={require("../images/KB logo.png")} />*/}
+      <div className={Mobile ? WebNavHeaderCSS.middleResponsive : WebNavHeaderCSS.middle} id="middle">
+        <div className={WebNavHeaderCSS.headerTitleContainer}>
+          <div className={WebNavHeaderCSS.headerKingBengals}>
           KingBengals</div>
-          <div className={NavBarUnderLayerCSS.headerCattery}>Cattery</div>
-          <div className={NavBarUnderLayerCSS.rightImgContainer}>
-            <img id={NavBarUnderLayerCSS.rightHeaderImg} alt='cat' src={require("../images/CatWebNavHeader2NoBackground.png")} />
+          <div className={WebNavHeaderCSS.headerCattery}>Cattery</div>
+          <div className={WebNavHeaderCSS.rightImgContainer}>
+            <img id={WebNavHeaderCSS.rightHeaderImg} alt='cat' src={require("../images/CatWebNavHeader2NoBackground.png")} />
           </div>  
         </div>
-        <div className={NavBarUnderLayerCSS.middleNavLinks} id="middleNavLinks">
-          <div className={Mobile ? NavBarUnderLayerCSS.navLinksResponsive : NavBarUnderLayerCSS.navLinks} id="navLinks">
-            <div className={NavBarUnderLayerCSS.link}>
+        <div className={WebNavHeaderCSS.middleNavLinks} id="middleNavLinks">
+          <div className={Mobile ? WebNavHeaderCSS.navLinksResponsive : WebNavHeaderCSS.navLinks} id="navLinks">
+            <div className={WebNavHeaderCSS.link}>
               <NavLink to="/home" 
-                className={(navData) => navData.isActive ? NavBarUnderLayerCSS.active : ""}>
+                className={(navData) => navData.isActive ? WebNavHeaderCSS.active : ""}>
                   Home</NavLink>
             </div>
-            <div className={NavBarUnderLayerCSS.link}>
+            <div className={WebNavHeaderCSS.link}>
               <NavLink to="/reserveakitten" 
-                className={(navData) => navData.isActive ? NavBarUnderLayerCSS.active : ""}>
+                className={(navData) => navData.isActive ? WebNavHeaderCSS.active : ""}>
                   ReserveKitten</NavLink>
             </div>
-            <div className={NavBarUnderLayerCSS.link}>
+            <div className={WebNavHeaderCSS.link}>
               <NavLink to="/about" 
-                className={(navData) => navData.isActive ? NavBarUnderLayerCSS.active : ""}>
+                className={(navData) => navData.isActive ? WebNavHeaderCSS.active : ""}>
                   About</NavLink>
             </div>
-            <div className={NavBarUnderLayerCSS.link}>
+            <div className={WebNavHeaderCSS.link}>
               <NavLink to="/ourcats" 
-                className={(navData) => navData.isActive ? NavBarUnderLayerCSS.active : ""}>
+                className={(navData) => navData.isActive ? WebNavHeaderCSS.active : ""}>
                   Cats</NavLink>
             </div>
-            <div className={NavBarUnderLayerCSS.link}>
+            <div className={WebNavHeaderCSS.link}>
               <NavLink to="/litterupdates" 
-                className={(navData) => navData.isActive ? NavBarUnderLayerCSS.active : ""}>
+                className={(navData) => navData.isActive ? WebNavHeaderCSS.active : ""}>
                   Updates</NavLink>
             </div>
-            <div className={NavBarUnderLayerCSS.link}>
+            <div className={WebNavHeaderCSS.link}>
               <NavLink to="/ownersinfo" 
-                className={(navData) => navData.isActive ? NavBarUnderLayerCSS.active : ""}>
+                className={(navData) => navData.isActive ? WebNavHeaderCSS.active : ""}>
                   Owners</NavLink>
             </div>
-            <div className={NavBarUnderLayerCSS.link}>
+            <div className={WebNavHeaderCSS.link}>
               <NavLink to="/contact" 
-                className={(navData) => navData.isActive ? NavBarUnderLayerCSS.active : ""}>
+                className={(navData) => navData.isActive ? WebNavHeaderCSS.active : ""}>
                   Contact</NavLink>
             </div>
-            <div className={NavBarUnderLayerCSS.link}>
+            <div className={WebNavHeaderCSS.link}>
               <NavLink to="/breedinfo" 
-                className={(navData) => navData.isActive ? NavBarUnderLayerCSS.active : ""}>
+                className={(navData) => navData.isActive ? WebNavHeaderCSS.active : ""}>
                   Breed</NavLink>
             </div>
           </div>
         </div>
         {/*<h5> Home ReserveAKitten AboutUs OurCats LitterUpdates InfoForOwners ContactUs BreedInfo</h5>*/}
   </div> 
-        <button className={NavBarUnderLayerCSS.navBtn} onClick={checkResponsiveView}>
+        <button className={WebNavHeaderCSS.navBtn} onClick={checkResponsiveView}>
             {Mobile ? <FaTimes /> : <FaBars />}
         </button>
       
