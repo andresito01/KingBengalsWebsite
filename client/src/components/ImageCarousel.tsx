5754d1ca2638f1f79db7fa94ee333fb80fa8c9ed
benpennington692 <benpennington692@gmail.com>
2023-05-12 09:31:39 -0700

    Image Carousel changes

--- client/src/components/ImageCarousel.tsx
+++ client/src/components/ImageCarousel.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import React from 'react';
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
-import "./styles/ImageCarousel.css"
+//import "./styles/ImageCarousel.css"
 import cat1 from "../images/catImage26.jpg"
 import cat2 from "../images/catImage24.jpg"
 import cat3 from "../images/catImage30.jpg"
@@ -10,53 +10,30 @@ import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import imageCarouselImages from  "../json/imageCarousel.json"
-
-const images = [cat1, cat2, cat3];
+import Carousel from 'react-bootstrap/Carousel';
 
 const ImageCarousel = () => {
-
-   //Owl Carousel Settings
-   const options = {
-    loop: true,
-    center: true,
-    items: 9,
-    margin: 0,
-    autoplay: true,
-    dots: true,
-    autoplayTimeout: 5000,
-    smartSpeed: 350,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 3
-        }
-    }
-  };
-
+  console.log(imageCarouselImages)
   return (
-    <div className='container'>
-      <OwlCarousel id="catteryImgs" className="owl-carousel owl-theme" {...options}>
-        {
-          imageCarouselImages.length === 0 ?
-            <div className="item">
-              <h3>No Images</h3>
-            </div> :
-            imageCarouselImages.map((catImg, index) => {
-              return (
-                <div className=" item" key={index}>
-                   <img className="itemImg" alt="sliderImg" src={catImg.image} />
-                </div>
-              )
-            })
-        }
-      </OwlCarousel>             
-    </div>
+    <Carousel fade style={{height:"20rem", width:"90%", display:"flex",alignItems:"center"}}>
+      {
+        imageCarouselImages.length === 0 ? null :
+        imageCarouselImages.map((catImg, index) => {
+          return (
+           
+              <Carousel.Item style={{height:"auto", width:"200px", position:"relative", display:"flex", alignItems:"center", marginLeft: "32%"}}>
+                <img
+                  className="itemImg d-block w-100"
+                  src={catImg.image}
+                  alt="sliderImg"
+                  style={{scale:"0.7"}}
+                />
+              </Carousel.Item>
+            
+          )
+        })
+      }
+    </Carousel>
   );
 }
 
